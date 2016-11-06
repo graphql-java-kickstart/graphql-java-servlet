c1abded252d2a155c8dd9119761546d12289d14e
Yurii Rashkovskii <yrashk@gmail.com>
2016-11-06 12:30:42 -0800

    Problem: object variables are decoded as maps
    However, it'd be really useful to get them back as original
    classes to be able to use them directly.
    
    Solution: use annotations' 0.13.1 capability to retrieve
    the original class

---
+++ src/main/java/graphql/servlet/GraphQLVariables.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2016 Yurii Rashkovskii
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ */
+package graphql.servlet;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import graphql.annotations.GraphQLObjectTypeWrapper;
+import graphql.language.OperationDefinition;
+import graphql.language.TypeName;
+import graphql.language.VariableDefinition;
+import graphql.parser.Parser;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.GraphQLType;
+import lombok.SneakyThrows;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class GraphQLVariables extends HashMap<String, Object> {
+
+    private final GraphQLSchema schema;
+    private final String query;
+
+    public GraphQLVariables(GraphQLSchema schema, String query, Map<String, Object> variables) {
+        super();
+        this.schema = schema;
+        this.query = query;
+        ObjectMapper objectMapper = new ObjectMapper();
+        new Parser().parseDocument(query).getDefinitions().stream()
+                    .filter(d -> d instanceof OperationDefinition)
+                    .map(d -> (OperationDefinition) d)
+                    .flatMap(d -> d.getVariableDefinitions().stream())
+                    .forEach(new Consumer<VariableDefinition>() {
+                        @SneakyThrows
+                        @Override public void accept(VariableDefinition d) {
+                            GraphQLType type = schema.getType(((TypeName) (d.getType())).getName());
+                            if (type instanceof GraphQLObjectTypeWrapper) {
+                                String value = objectMapper.writeValueAsString(variables.get(d.getName()));
+                                Object val = objectMapper.readValue(value, ((GraphQLObjectTypeWrapper) type).getObjectClass());
+                                GraphQLVariables.this.put(d.getName(), val);
+                            } else {
+                                GraphQLVariables.this.put(d.getName(), variables.get(d.getName()));
+                            }
+                        }
+                    });
+    }
+}
---
+++ src/test/java/graphql/servlet/GraphQLVariablesTest.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2016 Yurii Rashkovskii
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ */
+package graphql.servlet;
+
+import graphql.annotations.GraphQLAnnotations;
+import graphql.annotations.GraphQLField;
+import graphql.annotations.GraphQLName;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
+import lombok.SneakyThrows;
+import lombok.Value;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class GraphQLVariablesTest {
+
+    public static class ComplexQueryProvider implements GraphQLQueryProvider {
+
+
+        @Value
+        public static class Data {
+            @GraphQLField
+            private String field1;
+            @GraphQLField
+            private String field2;
+        }
+
+        @GraphQLName("data")
+        public static class DataQuery {
+            @GraphQLField
+            public Data echo(Data data) {
+                return data;
+            }
+        }
+
+        @Override @SneakyThrows
+        public GraphQLObjectType getQuery() {
+            return GraphQLAnnotations.object(DataQuery.class);
+        }
+
+        @Override
+        public Object context() {
+            return new DataQuery();
+        }
+    }
+
+    private static final String QUERY = "query Q($d: Data) { data { echo(data: $d) { field1 field2 } } }";
+
+    @Test
+    public void variableTyping() {
+        GraphQLServlet servlet = new GraphQLServlet();
+        ComplexQueryProvider queryProvider = new ComplexQueryProvider();
+        servlet.bindQueryProvider(queryProvider);
+        GraphQLSchema schema = servlet.getSchema();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("field1", "1");
+        data.put("field2", "2");
+        HashMap<String, Object> vars = new HashMap<>();
+        vars.put("d", data);
+        GraphQLVariables variables = new GraphQLVariables(schema, QUERY, vars);
+        Object d = variables.get("d");
+        assertTrue(d instanceof ComplexQueryProvider.Data);
+        assertEquals(((ComplexQueryProvider.Data)d).getField1(), "1");
+        assertEquals(((ComplexQueryProvider.Data)d).getField2(), "2");
+    }
+}
