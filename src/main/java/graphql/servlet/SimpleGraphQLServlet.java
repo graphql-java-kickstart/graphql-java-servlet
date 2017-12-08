574d816cfd928832af9758b64ada711506ac171d
Justin Plock <jplock@users.noreply.github.com>
2017-12-08 10:23:50 -0500

    Add a builder to SimpleGraphQLServlet (#53)
    * Ignore eclipse files
    
    * Add a builder to SimpleGraphQLServlet
    
    * Add a create() method

--- src/main/java/graphql/servlet/SimpleGraphQLServlet.java
+++ src/main/java/graphql/servlet/SimpleGraphQLServlet.java
@@ -17,22 +17,42 @@ import java.util.Optional;
  */
 public class SimpleGraphQLServlet extends GraphQLServlet {
 
+    /**
+     * @deprecated use {@link #builder()} instead.
+     */
+    @Deprecated
     public SimpleGraphQLServlet(GraphQLSchema schema) {
         this(schema, new DefaultExecutionStrategyProvider());
     }
 
+    /**
+     * @deprecated use {@link #builder()} instead.
+     */
+    @Deprecated
     public SimpleGraphQLServlet(GraphQLSchema schema, ExecutionStrategy executionStrategy) {
         this(schema, new DefaultExecutionStrategyProvider(executionStrategy));
     }
 
+    /**
+     * @deprecated use {@link #builder()} instead.
+     */
+    @Deprecated
     public SimpleGraphQLServlet(GraphQLSchema schema, ExecutionStrategyProvider executionStrategyProvider) {
         this(schema, executionStrategyProvider, null, null, null, null, null, null, null);
     }
 
+    /**
+     * @deprecated use {@link #builder()} instead.
+     */
+    @Deprecated
     public SimpleGraphQLServlet(final GraphQLSchema schema, ExecutionStrategyProvider executionStrategyProvider, ObjectMapperConfigurer objectMapperConfigurer, List<GraphQLServletListener> listeners, Instrumentation instrumentation, GraphQLErrorHandler errorHandler, GraphQLContextBuilder contextBuilder, GraphQLRootObjectBuilder rootObjectBuilder, PreparsedDocumentProvider preparsedDocumentProvider) {
         this(new DefaultGraphQLSchemaProvider(schema), executionStrategyProvider, objectMapperConfigurer, listeners, instrumentation, errorHandler, contextBuilder, rootObjectBuilder, preparsedDocumentProvider);
     }
 
+    /**
+     * @deprecated use {@link #builder()} instead.
+     */
+    @Deprecated
     public SimpleGraphQLServlet(GraphQLSchemaProvider schemaProvider, ExecutionStrategyProvider executionStrategyProvider, ObjectMapperConfigurer objectMapperConfigurer, List<GraphQLServletListener> listeners, Instrumentation instrumentation, GraphQLErrorHandler errorHandler, GraphQLContextBuilder contextBuilder, GraphQLRootObjectBuilder rootObjectBuilder, PreparsedDocumentProvider preparsedDocumentProvider) {
         super(objectMapperConfigurer, listeners, null);
 
@@ -70,6 +90,18 @@ public class SimpleGraphQLServlet extends GraphQLServlet {
         }
     }
 
+    private SimpleGraphQLServlet(Builder builder) {
+        super(builder.objectMapperConfigurer, builder.listeners, null);
+
+        this.schemaProvider = builder.schemaProvider;
+        this.executionStrategyProvider = builder.executionStrategyProvider;
+        this.instrumentation = builder.instrumentation;
+        this.errorHandler = builder.errorHandler;
+        this.contextBuilder = builder.contextBuilder;
+        this.rootObjectBuilder = builder.rootObjectBuilder;
+        this.preparsedDocumentProvider = builder.preparsedDocumentProvider;
+    }
+
     private final GraphQLSchemaProvider schemaProvider;
     private final ExecutionStrategyProvider executionStrategyProvider;
     private final Instrumentation instrumentation;
@@ -77,6 +109,69 @@ public class SimpleGraphQLServlet extends GraphQLServlet {
     private final GraphQLContextBuilder contextBuilder;
     private final GraphQLRootObjectBuilder rootObjectBuilder;
     private final PreparsedDocumentProvider preparsedDocumentProvider;
+
+    public static SimpleGraphQLServlet create(GraphQLSchema schema) {
+        return new Builder(schema).build();
+    }
+
+    public static Builder builder(GraphQLSchema schema) {
+        return new Builder(schema);
+    }
+
+    public static class Builder {
+        private final GraphQLSchemaProvider schemaProvider;
+        private ExecutionStrategyProvider executionStrategyProvider = new DefaultExecutionStrategyProvider();
+        private ObjectMapperConfigurer objectMapperConfigurer;
+        private List<GraphQLServletListener> listeners;
+        private Instrumentation instrumentation = NoOpInstrumentation.INSTANCE;
+        private GraphQLErrorHandler errorHandler = new DefaultGraphQLErrorHandler();
+        private GraphQLContextBuilder contextBuilder = new DefaultGraphQLContextBuilder();
+        private GraphQLRootObjectBuilder rootObjectBuilder = new DefaultGraphQLRootObjectBuilder();
+        private PreparsedDocumentProvider preparsedDocumentProvider = NoOpPreparsedDocumentProvider.INSTANCE;
+
+        public Builder(GraphQLSchema schema) {
+            this.schemaProvider = new DefaultGraphQLSchemaProvider(schema);
+        }
+
+        public Builder withExecutionStrategyProvider(ExecutionStrategyProvider provider) {
+            this.executionStrategyProvider = provider;
+            return this;
+        }
+
+        public Builder withObjectMapperConfigurer(ObjectMapperConfigurer configurer) {
+            this.objectMapperConfigurer = configurer;
+            return this;
+        }
+
+        public Builder withInstrumentation(Instrumentation instrumentation) {
+            this.instrumentation = instrumentation;
+            return this;
+        }
+
+        public Builder withGraphQLErrorHandler(GraphQLErrorHandler handler) {
+            this.errorHandler = handler;
+            return this;
+        }
+
+        public Builder withGraphQLContextBuilder(GraphQLContextBuilder context) {
+            this.contextBuilder = context;
+            return this;
+        }
+
+        public Builder withGraphQLRootObjectBuilder(GraphQLRootObjectBuilder rootObject) {
+            this.rootObjectBuilder = rootObject;
+            return this;
+        }
+
+        public Builder withPreparsedDocumentProvider(PreparsedDocumentProvider provider) {
+            this.preparsedDocumentProvider = provider;
+            return this;
+        }
+
+        public SimpleGraphQLServlet build() {
+            return new SimpleGraphQLServlet(this);
+        }
+    }
 
     @Override
     protected GraphQLSchemaProvider getSchemaProvider() {
