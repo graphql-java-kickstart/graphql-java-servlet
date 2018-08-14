6c264c056ce16e958ea5a99ec1fb8a0898cf7217
Max Bruchmann <bruchmann@signal7.de>
2018-08-14 19:07:52 +0200

    Introduce ObjectMapperProvider
    Default Provider that works like the initial implementation

---
+++ src/main/java/graphql/servlet/ConfiguringObjectMapperProvider.java
@@ -0,0 +1,32 @@
+package graphql.servlet;
+
+import com.fasterxml.jackson.databind.InjectableValues;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
+public class ConfiguringObjectMapperProvider implements ObjectMapperProvider {
+
+  private final ObjectMapperConfigurer objectMapperConfigurer;
+
+  public ConfiguringObjectMapperProvider(ObjectMapperConfigurer objectMapperConfigurer) {
+    this.objectMapperConfigurer = objectMapperConfigurer;
+  }
+
+  public ConfiguringObjectMapperProvider() {
+    this.objectMapperConfigurer = new DefaultObjectMapperConfigurer();
+  }
+
+  @Override
+  public ObjectMapper provide() {
+    ObjectMapper mapper = new ObjectMapper().disable(
+      SerializationFeature.FAIL_ON_EMPTY_BEANS).registerModule(new Jdk8Module());
+    objectMapperConfigurer.configure(mapper);
+
+    InjectableValues.Std injectableValues = new InjectableValues.Std();
+    injectableValues.addValue(ObjectMapper.class, mapper);
+    mapper.setInjectableValues(injectableValues);
+
+    return mapper;
+  }
+}
