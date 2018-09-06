90eb3ed474c8b161ce2da4af720f8cb7821c2e56
Michiel Oliemans <oliemans@scopisto.com>
2018-09-06 08:14:26 +0200

    Added comment to keep constructor protected

--- src/main/java/graphql/servlet/SimpleGraphQLHttpServlet.java
+++ src/main/java/graphql/servlet/SimpleGraphQLHttpServlet.java
@@ -13,6 +13,7 @@ public class SimpleGraphQLHttpServlet extends AbstractGraphQLHttpServlet {
     private final GraphQLQueryInvoker queryInvoker;
     private final GraphQLObjectMapper graphQLObjectMapper;
 
+    // protected to allow class to be extended
     protected SimpleGraphQLHttpServlet(GraphQLInvocationInputFactory invocationInputFactory, GraphQLQueryInvoker queryInvoker, GraphQLObjectMapper graphQLObjectMapper, List<GraphQLServletListener> listeners, boolean asyncServletMode) {
         super(listeners, asyncServletMode);
         this.invocationInputFactory = invocationInputFactory;
