2d825c40186586b96cd6d534c0362a83c74b992a
Michiel Oliemans <oliemans@scopisto.com>
2018-09-29 18:23:17 +0200

    Updated build settings for Travis

--- travis-build.sh
+++ travis-build.sh
@@ -3,5 +3,6 @@ set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ "${RELEASE}" = "true" ]; then
     echo "Deploying release to Bintray"
+    git checkout -f ${TRAVIS_BRANCH}
     ./gradlew clean assemble release -Prelease.useAutomaticVersion=true && ./gradlew check --info && ./gradlew bintrayUpload -x check --info
 fi
