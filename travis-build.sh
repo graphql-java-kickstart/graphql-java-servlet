0b1ff5f5141ca18d340ec7248245b93f07691502
Michiel Oliemans <oliemans@scopisto.com>
2018-09-29 18:57:23 +0200

    Updated build settings for Travis

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -ev
 
+saveGitCredentials() {
+    cat >$HOME/.netrc <<EOL
+machine github.com
+login ${GITHUB_USERNAME}
+password ${GITHUB_TOKEN}
+
+machine api.github.com
+login ${GITHUB_USERNAME}
+password ${GITHUB_TOKEN}
+EOL
+    chmod 600 $HOME/.netrc
+}
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ "${RELEASE}" = "true" ]; then
     echo "Deploying release to Bintray"
+    saveGitCredentials
     git checkout -f ${TRAVIS_BRANCH}
     ./gradlew clean assemble release -Prelease.useAutomaticVersion=true && ./gradlew check --info
 fi
