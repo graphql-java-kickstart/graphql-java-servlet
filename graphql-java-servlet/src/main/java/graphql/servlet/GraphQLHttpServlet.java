2e934647d61ae82082bb65869ea74cef40e83f7e
Michiel Oliemans <oliemans@scopisto.com>
2018-11-24 10:05:44 +0100

    Improved usage of refactored GraphQLContext

---
+++ graphql-java-servlet/src/main/java/graphql/servlet/GraphQLHttpServlet.java
@@ -0,0 +1,21 @@
+package graphql.servlet;
+
+import graphql.schema.GraphQLSchema;
+
+/**
+ * @author Michiel Oliemans
+ */
+public abstract class GraphQLHttpServlet extends AbstractGraphQLHttpServlet {
+
+    public static GraphQLHttpServlet with(GraphQLSchema schema) {
+        return new ConfiguredGraphQLHttpServlet(GraphQLConfiguration.with(schema).build());
+    }
+
+    public static GraphQLHttpServlet with(GraphQLConfiguration configuration) {
+        return new ConfiguredGraphQLHttpServlet(configuration);
+    }
+
+    @Override
+    protected abstract GraphQLConfiguration getConfiguration();
+
+}
