15f5fb4e6ccea5b7b97343e15fb926d62830dea3
John McCaulley <john.mccaulley@walmart.com>
2019-02-27 14:30:38 -0500

    javadoc

--- src/main/java/graphql/servlet/BatchExecutionHandler.java
+++ src/main/java/graphql/servlet/BatchExecutionHandler.java
@@ -10,10 +10,13 @@ import java.util.function.BiFunction;
  * @author Andrew Potter
  */
 public interface BatchExecutionHandler {
+
     /**
      * Allows separating the logic of handling batch queries from how each individual query is resolved.
      * @param batchedInvocationInput the batch query input
      * @param queryFunction Function to produce query results.
+     * @param graphQLObjectMapper object mapper used to serialize results
+     * @param writer request writer to ouput results.
      */
     void handleBatch(GraphQLBatchedInvocationInput batchedInvocationInput, Writer writer, GraphQLObjectMapper graphQLObjectMapper,
                      BiFunction<GraphQLInvocationInput, ExecutionInput, ExecutionResult> queryFunction);
