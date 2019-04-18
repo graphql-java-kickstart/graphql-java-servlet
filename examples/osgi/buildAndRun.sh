2d89c929a604d30e22b7aecd2d3fcb499f2d40c3
Serge Huber <shuber@jahia.com>
2019-04-18 21:48:33 +0200

    Update Example to latest versions

--- examples/osgi/buildAndRun.sh
+++ examples/osgi/buildAndRun.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mvn clean install
 pushd apache-karaf-package/target
-tar zxvf graphql-java-servlet-osgi-examples-apache-karaf-package-3.0.1.tar.gz
-cd graphql-java-servlet-osgi-examples-apache-karaf-package-3.0.1/bin
+tar zxvf graphql-java-servlet-osgi-examples-apache-karaf-package-7.3.4-SNAPSHOT.tar.gz
+cd graphql-java-servlet-osgi-examples-apache-karaf-package-7.3.4-SNAPSHOT/bin
 ./karaf debug
 popd
