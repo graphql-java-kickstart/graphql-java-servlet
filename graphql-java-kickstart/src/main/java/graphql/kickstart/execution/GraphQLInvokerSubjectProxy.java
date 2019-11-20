3dbad111015df8ea8b2b7625f6da11cefe85a97a
Michiel Oliemans <oliemans@scopisto.com>
2019-11-20 08:10:37 +0100

    Fix build error

--- graphql-java-kickstart/src/main/java/graphql/kickstart/execution/GraphQLInvokerSubjectProxy.java
+++ graphql-java-kickstart/src/main/java/graphql/kickstart/execution/GraphQLInvokerSubjectProxy.java
@@ -2,6 +2,7 @@ package graphql.kickstart.execution;
 
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
+import graphql.GraphQL;
 import graphql.kickstart.execution.context.GraphQLContext;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
@@ -11,19 +12,19 @@ import javax.security.auth.Subject;
 public class GraphQLInvokerSubjectProxy implements GraphQLInvokerProxy {
 
   @Override
-  public CompletableFuture<ExecutionResult> executeAsync(GraphQLInvoker graphQLInvoker, ExecutionInput executionInput) {
+  public CompletableFuture<ExecutionResult> executeAsync(GraphQL graphQL, ExecutionInput executionInput) {
     GraphQLContext context = (GraphQLContext) executionInput.getContext();
     if (Subject.getSubject(AccessController.getContext()) == null && context.getSubject().isPresent()) {
       return Subject
           .doAs(context.getSubject().get(), (PrivilegedAction<CompletableFuture<ExecutionResult>>) () -> {
             try {
-              return graphQLInvoker.executeAsync(executionInput);
+              return graphQL.executeAsync(executionInput);
             } catch (Exception e) {
               throw new RuntimeException(e);
             }
           });
     }
-    return graphQLInvoker.executeAsync(executionInput);
+    return graphQL.executeAsync(executionInput);
   }
 
 }
