2bb6d7d5f583e88e9effe6ea8678513c76ff0b86
Michiel Oliemans <oliemans@scopisto.com>
2020-04-07 09:06:39 +0200

    Update build script for publishing snapshots

--- travis-build.sh
+++ travis-build.sh
@@ -14,17 +14,63 @@ EOL
     chmod 600 $HOME/.netrc
 }
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ "${RELEASE}" = "true" ]; then
+getVersion() {
+  ./gradlew properties -q | grep "version:" | grep -v "kotlin_version:" | awk '{print $2}' | tr -d '[:space:]'
+}
+
+removeSnapshots() {
+  sed -i 's/-SNAPSHOT//' gradle.properties
+}
+
+commitRelease() {
+  local APP_VERSION=$(getVersion)
+  git commit -a -m "Update version for release"
+  git tag -a "v${APP_VERSION}" -m "Tag release version"
+}
+
+bumpVersion() {
+  echo "Bump version number"
+  local APP_VERSION=$(getVersion | xargs)
+  local SEMANTIC_REGEX='^([0-9]+)\.([0-9]+)(\.([0-9]+))?$'
+  if [[ ${APP_VERSION} =~ ${SEMANTIC_REGEX} ]]; then
+    if [[ ${BASH_REMATCH[4]} ]]; then
+      nextVersion=$((BASH_REMATCH[4] + 1))
+      nextVersion="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${nextVersion}-SNAPSHOT"
+    else
+      nextVersion=$((BASH_REMATCH[2] + 1))
+      nextVersion="${BASH_REMATCH[1]}.${nextVersion}-SNAPSHOT"
+    fi
+
+    echo "Next version: ${nextVersion}"
+    sed -i -E "s/^version(\s)?=.*/version=${nextVersion}/" gradle.properties
+  else
+    echo "No semantic version and therefore cannot publish to maven repository: '${APP_VERSION}'"
+  fi
+}
+
+commitNextVersion() {
+  git commit -a -m "Update version for release"
+}
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+  if [ "${RELEASE}" = "true" ]; then
+    echo "Deploying release to Bintray"
     saveGitCredentials
     git checkout -f ${TRAVIS_BRANCH}
-    if [ "${RELEASE}" = "true" ]; then
-        echo "Deploying release to Bintray"
-#        ./gradlew clean assemble release -Prelease.useAutomaticVersion=true && ./gradlew check --info
-        ./gradlew clean assemble && ./gradlew check --info && ./gradlew bintrayUpload -x check --info
-    else
-        echo "Deploying snapshot to Bintray"
-        ./gradlew artifactoryPublish && ./gradlew check --info
-    fi
+    removeSnapshots
+
+    ./gradlew clean assemble && ./gradlew check --info && ./gradlew bintrayUpload -x check --info
+
+    commitRelease
+    bumpVersion
+    commitNextVersion
+    git push --follow-tags
+  else
+    echo "Deploying snapshot"
+    saveGitCredentials
+    ./gradlew artifactoryPublish -Dsnapshot=true -Dbuild.number="${TRAVIS_BUILD_NUMBER}"
+  fi
 else
-    ./gradlew check --info
+    echo "Verify"
+    ./gradlew clean assemble && ./gradlew check --info
 fi
