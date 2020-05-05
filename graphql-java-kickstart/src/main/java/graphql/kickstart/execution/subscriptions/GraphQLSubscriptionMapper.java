836c97233729201b394dd6511647a730077b901c
Bogdan Kobylynskyi <92bogdan@gmail.com>
2020-05-04 22:17:42 -0500

    Fix Subscription GraphQLRequest deserialization

--- graphql-java-kickstart/src/main/java/graphql/kickstart/execution/subscriptions/GraphQLSubscriptionMapper.java
+++ graphql-java-kickstart/src/main/java/graphql/kickstart/execution/subscriptions/GraphQLSubscriptionMapper.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import graphql.ExecutionResult;
 import graphql.kickstart.execution.GraphQLObjectMapper;
 import graphql.kickstart.execution.GraphQLRequest;
+import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,7 +15,12 @@ public class GraphQLSubscriptionMapper {
   private final GraphQLObjectMapper graphQLObjectMapper;
 
   public GraphQLRequest readGraphQLRequest(Object payload) {
-    return graphQLObjectMapper.getJacksonMapper().convertValue(payload, GraphQLRequest.class);
+    Objects.requireNonNull(payload, "Payload is required");
+    try {
+      return graphQLObjectMapper.readGraphQLRequest(payload.toString());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public ExecutionResult sanitizeErrors(ExecutionResult executionResult) {
