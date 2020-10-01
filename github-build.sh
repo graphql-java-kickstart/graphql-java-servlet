59aa3fad04f5b4ad81ddd5811b7e221127ed376c
Michiel Oliemans <oliemans@scopisto.com>
2020-10-01 12:06:57 +0200

    Try to fix git commit issue

--- github-build.sh
+++ github-build.sh
@@ -52,6 +52,9 @@ commitNextVersion() {
   git commit -a -m "Update version for release"
 }
 
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Actions"
+
 echo "Deploying release to Bintray"
 removeSnapshots
 
