6a52a3849c26ef923fca40b60901496a274171e6
Artem Grachev <65827946+artem-ag@users.noreply.github.com>
2020-12-29 11:59:03 -0800

    Optimize single & batch query response writers to reduce memory allocation overhead caused by the servlet.

---
+++ graphql-java-servlet/src/test/groovy/graphql/kickstart/servlet/BatchedQueryResponseWriterTest.groovy
@@ -0,0 +1,55 @@
+package graphql.kickstart.servlet
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import graphql.ExecutionResultImpl
+import graphql.kickstart.execution.GraphQLObjectMapper
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.servlet.ServletOutputStream
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import java.nio.charset.StandardCharsets
+
+class BatchedQueryResponseWriterTest extends Specification {
+
+    @Unroll
+    def "should write utf8 results into the response with content #result"() {
+        given:
+        def byteArrayOutputStream = new ByteArrayOutputStream()
+        def graphQLObjectMapperMock = GraphQLObjectMapper.newBuilder().withObjectMapperProvider({ new ObjectMapper() }).build()
+        graphQLObjectMapperMock.getJacksonMapper() >> new ObjectMapper()
+
+        def requestMock = Mock(HttpServletRequest)
+        def responseMock = Mock(HttpServletResponse)
+        def servletOutputStreamMock = Mock(ServletOutputStream)
+
+        responseMock.getOutputStream() >> servletOutputStreamMock
+
+        1 * responseMock.setContentLength(expectedContentLengh)
+        1 * responseMock.setCharacterEncoding(StandardCharsets.UTF_8.name())
+        (1.._) * servletOutputStreamMock.write(_) >> { value ->
+            byteArrayOutputStream.write((byte[])(value[0]))
+        }
+
+        def executionResultList = new ArrayList()
+        for (LinkedHashMap<Object, Object> value : result) {
+            executionResultList.add(new ExecutionResultImpl(value, []))
+        }
+
+        def writer = new BatchedQueryResponseWriter(executionResultList, graphQLObjectMapperMock)
+
+        when:
+        writer.write(requestMock, responseMock)
+
+        then:
+        byteArrayOutputStream.toString(StandardCharsets.UTF_8) == expectedResponseContent
+
+        where:
+        result                      || expectedContentLengh | expectedResponseContent
+        [[testValue: "abcde"]]      || 32                   | """[{"data":{"testValue":"abcde"}}]"""
+        [[testValue: "äöüüöß"]]     || 39                   | """[{"data":{"testValue":"äöüüöß"}}]"""
+        []                          || 2                    | """[]"""
+        [[k1: "äöüüöß"], [k2: "a"]] || 52                   | """[{"data":{"k1":"äöüüöß"}},{"data":{"k2":"a"}}]"""
+    }
+}
