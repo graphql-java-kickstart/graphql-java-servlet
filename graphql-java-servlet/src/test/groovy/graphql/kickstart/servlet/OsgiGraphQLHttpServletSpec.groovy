e77caf02209cf39c615303c09fd32e3ce86dc70c
Thomas Draier <tdraier@jahia.com>
2023-09-15 19:08:22 +0200

    Restore inheritance

--- graphql-java-servlet/src/test/groovy/graphql/kickstart/servlet/OsgiGraphQLHttpServletSpec.groovy
+++ graphql-java-servlet/src/test/groovy/graphql/kickstart/servlet/OsgiGraphQLHttpServletSpec.groovy
@@ -70,7 +70,7 @@ class OsgiGraphQLHttpServletSpec extends Specification {
     servlet.getConfiguration().getInvocationInputFactory().getSchemaProvider().getReadOnlySchema().getQueryType().getFieldDefinitions().get(0).name == "_empty"
   }
 
-  static class TestMutationProvider implements GraphQLMutationProvider, GraphQLProvider {
+  static class TestMutationProvider implements GraphQLMutationProvider {
     @Override
     Collection<GraphQLFieldDefinition> getMutations() {
       return Collections.singletonList(newFieldDefinition().name("int").type(GraphQLInt).staticValue(1).build())
@@ -105,7 +105,7 @@ class OsgiGraphQLHttpServletSpec extends Specification {
     servlet.getConfiguration().getInvocationInputFactory().getSchemaProvider().getSchema().getMutationType() == null
   }
 
-  static class TestSubscriptionProvider implements GraphQLSubscriptionProvider, GraphQLProvider {
+  static class TestSubscriptionProvider implements GraphQLSubscriptionProvider {
     @Override
     Collection<GraphQLFieldDefinition> getSubscriptions() {
       return Collections.singletonList(newFieldDefinition().name("subscription").type(new GraphQLAnnotations().object(Subscription.class)).build())
