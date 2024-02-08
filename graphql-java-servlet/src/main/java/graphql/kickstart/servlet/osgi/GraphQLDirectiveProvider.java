d9d2d1d22e7c2de3beeb4a2bc0da99db5d5a538b
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2024-02-07 19:20:03 -0500

    Switch getDirectives to return Collection instead
    Co-authored-by: Federico Rispo <dev@federicorispo.com>

--- graphql-java-servlet/src/main/java/graphql/kickstart/servlet/osgi/GraphQLDirectiveProvider.java
+++ graphql-java-servlet/src/main/java/graphql/kickstart/servlet/osgi/GraphQLDirectiveProvider.java
@@ -1,12 +1,12 @@
 package graphql.kickstart.servlet.osgi;
 
 import graphql.schema.GraphQLDirective;
-import java.util.Set;
+import java.util.Collection;
 
 
 public interface GraphQLDirectiveProvider extends GraphQLProvider {
 
     /** @return A collection of directive definitions that will be added to the schema. */
-    Set<GraphQLDirective> getDirectives();
+    Collection<GraphQLDirective> getDirectives();
 
 }
